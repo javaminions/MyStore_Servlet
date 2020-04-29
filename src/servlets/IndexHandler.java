642962c5be7344439a288a6120c3df2399ffcd50
chris <chris@192.168.10.101>
2020-04-29 15:39:59 -0700

    end of day commit

--- src/servlets/IndexHandler.java
+++ src/servlets/IndexHandler.java
@@ -16,10 +16,23 @@ public class IndexHandler extends HttpServlet {
 		System.out.println(navSelection);
 		if(navSelection.equalsIgnoreCase("profile")){
 			request.getRequestDispatcher("views/UserProfile.jsp").forward(request, response);
+		} 
+		if(navSelection.equalsIgnoreCase("orders")) {
+			request.getRequestDispatcher("views/Orders.jsp").forward(request, response);
+		}
+		if(navSelection.equalsIgnoreCase("cart")) {
+			request.getRequestDispatcher("views/").forward(request, response);
+		}
+		if(navSelection.equalsIgnoreCase("logout")) {
+			request.getRequestDispatcher("views/").forward(request, response);
 		}
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
+	}
+	
+	private void trafficDirector(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 	}
 
 }
