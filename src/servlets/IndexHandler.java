c7284966e97471f682aa7699fcf8c4c810ebc4fd
chris <chris@192.168.10.101>
2020-04-30 07:57:55 -0700

    Big Changes, plugged in just about everything via servlet

--- src/servlets/IndexHandler.java
+++ src/servlets/IndexHandler.java
@@ -14,6 +14,23 @@ public class IndexHandler extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String navSelection = request.getParameter("navSelection");
 		System.out.println(navSelection);
+		if(navSelection!=null) {
+			navHandler(navSelection, request, response);
+		}
+		
+		
+		String footerSelection = request.getParameter("footerSelection");
+		System.out.println(footerSelection);
+		if(footerSelection!=null) {
+			footerHandler(footerSelection, request, response);
+		}
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+	
+	private void navHandler(String navSelection, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		if(navSelection.equalsIgnoreCase("profile")){
 			request.getRequestDispatcher("views/UserProfile.jsp").forward(request, response);
 		} 
@@ -21,18 +38,36 @@ public class IndexHandler extends HttpServlet {
 			request.getRequestDispatcher("views/Orders.jsp").forward(request, response);
 		}
 		if(navSelection.equalsIgnoreCase("cart")) {
-			request.getRequestDispatcher("views/").forward(request, response);
+			request.getRequestDispatcher("views/Cart.jsp").forward(request, response);
 		}
 		if(navSelection.equalsIgnoreCase("logout")) {
-			request.getRequestDispatcher("views/").forward(request, response);
+			request.getRequestDispatcher("views/Logout.jsp").forward(request, response);
 		}
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
 	
-	private void trafficDirector(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void footerHandler(String footerSelection, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		if(footerSelection.equalsIgnoreCase("contactus")) {
+			request.getRequestDispatcher("views/ContactUs.jsp").forward(request, response);
+		}
+		if(footerSelection.equalsIgnoreCase("aboutus")) {
+			request.getRequestDispatcher("views/AboutUs.jsp").forward(request, response);
+		}
+		if(footerSelection.equalsIgnoreCase("testimonials")) {
+			request.getRequestDispatcher("views/Testimonials.jsp").forward(request, response);
+		}
+		if(footerSelection.equalsIgnoreCase("press")) {
+			request.getRequestDispatcher("views/Press.jsp").forward(request, response);
+		}
+		if(footerSelection.equalsIgnoreCase("payments")) {
+			request.getRequestDispatcher("views/Payments.jsp").forward(request, response);
+		}
+		if(footerSelection.equalsIgnoreCase("shipping")) {
+			request.getRequestDispatcher("views/Shipping.jsp").forward(request, response);
+		}
+		if(footerSelection.equalsIgnoreCase("return")) {
+			request.getRequestDispatcher("views/Return.jsp").forward(request, response);
+		}
 	}
 
 }
