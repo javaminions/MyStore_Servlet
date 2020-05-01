3bb8e97dde43390ef7f1115bb5ec745ef60900fa
chris <chris@192.168.10.101>
2020-05-01 10:03:47 -0700

    added annotation and removed web.xml

--- WebContent/views/cart.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	
-$(".cartButton").click(function() {
-	var count = parseInt($('#lblCartCount').text());
-		$('#lblCartCount').text(parseInt(count+=1));
-	});
-});
--- src/servlets/CategoryServlet.java
+++ src/servlets/CategoryServlet.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpSession;
 import pojo.Product;
 import pojo.ProductIO;
 
+@WebServlet("/categories")
 public class CategoryServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
