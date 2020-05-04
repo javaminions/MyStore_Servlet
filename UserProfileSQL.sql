95272b7abd23849498d4ff5894f37e688bb94369
chris <chris@192.168.10.101>
2020-05-04 13:29:09 -0700

    UserProfileSQL to create table

---
+++ UserProfileSQL.sql
@@ -0,0 +1,11 @@
+USE mystore_servlet;
+
+DROP TABLE IF EXISTS UserProfile;
+CREATE TABLE UserProfile (
+	firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    cartProducts VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
